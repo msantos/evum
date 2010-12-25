b49d969adadd596bdd71aedae8af4a52902652e3
Michael Santos <michael.santos@gmail.com>
2010-12-25 16:53:40 -0500

    Clean up after exit

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-exec erl -boot start_sasl -pa $PWD/ebin $PWD/deps/*/ebin
+erl -boot start_sasl -pa $PWD/ebin $PWD/deps/*/ebin
+pkill -TERM linux > /dev/null 2>&1
+rm priv/uml/evum.ctl > /dev/null 2>&1
